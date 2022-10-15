dba435b7893d6b88e29baf8f0519cbd0fd7279f6
Masato Kataoka <kata.masa@gmail.com>
2022-10-15 23:52:31 +0900

    ID指定の変更

--- environments/dev/igw.tf
+++ environments/dev/igw.tf
@@ -5,5 +5,5 @@ resource "aws_internet_gateway" "gw" {
     tags_all = {
         "Name" = "sbcntr-igw"
     }
-    vpc_id   = "vpc-0fde87b32cacadbc4"
+    vpc_id   = "${aws_vpc.vpc.id}"
 }
--- environments/dev/routetable.tf
+++ environments/dev/routetable.tf
@@ -7,7 +7,7 @@ resource "aws_route_table" "sbcntr-route-app" {
     tags_all         = {
         "Name" = "sbcntr-route-app"
     }
-    vpc_id           = "vpc-0fde87b32cacadbc4"
+    vpc_id           = "${aws_vpc.vpc.id}"
 }
 
 resource "aws_route_table" "sbcntr-route-db" {
@@ -19,14 +19,14 @@ resource "aws_route_table" "sbcntr-route-db" {
     tags_all         = {
         "Name" = "sbcntr-route-db"
     }
-    vpc_id           = "vpc-0fde87b32cacadbc4"
+    vpc_id           = "${aws_vpc.vpc.id}"
 }
 
 resource "aws_route_table" "sbcntr-route-ingress" {
     propagating_vgws = []
     route {
         cidr_block                 = "0.0.0.0/0"
-        gateway_id                 = "igw-08a0be89d95b322e7"
+        gateway_id                 = "${aws_internet_gateway.gw.id}"
     }
     tags             = {
         "Name" = "sbcntr-route-ingress"
@@ -34,62 +34,63 @@ resource "aws_route_table" "sbcntr-route-ingress" {
     tags_all         = {
         "Name" = "sbcntr-route-ingress"
     }
-    vpc_id           = "vpc-0fde87b32cacadbc4"
+    vpc_id           = "${aws_vpc.vpc.id}"
 }
 
 resource "aws_route" "sbcntr-route-app" {
     destination_cidr_block = "10.0.0.0/16"
     gateway_id             = "local"
-    route_table_id         = "rtb-075cb4cb775022237"
+    route_table_id         = "${aws_route_table.sbcntr-route-app.id}"
 }
 
 resource "aws_route" "sbcntr-route-db" {
     destination_cidr_block = "10.0.0.0/16"
     gateway_id             = "local"
-    route_table_id         = "rtb-0295d2910613f8d26"
+    route_table_id         = "${aws_route_table.sbcntr-route-db.id}"
 }
 
 resource "aws_route" "sbcntr-route-ingress" {
     destination_cidr_block = "10.0.0.0/16"
     gateway_id             = "local"
-    route_table_id         = "rtb-0d26b619f4cce982a"
+    route_table_id         = "${aws_route_table.sbcntr-route-ingress.id}"
 }
 
 resource "aws_route_table_association" "sbcntr-subnet-public-ingress-1a" {
-    route_table_id = "rtb-0d26b619f4cce982a"
-    subnet_id      = "subnet-03089260bcb2f8c56"
+    route_table_id = "${aws_route.sbcntr-route-ingress.id}"
+    subnet_id      = "${aws_subnet.sbcntr-subnet-public-ingress-1a.id}"
 }
 
 resource "aws_route_table_association" "sbcntr-subnet-public-ingress-1c" {
-    route_table_id = "rtb-0d26b619f4cce982a"
-    subnet_id      = "subnet-067e7d33312a751ab"
+    route_table_id = "${aws_route.sbcntr-route-ingress.id}"
+    subnet_id      = "${aws_subnet.sbcntr-subnet-public-ingress-1c.id}"
 }
 
 resource "aws_route_table_association" "sbcntr-subnet-private-container-1a" {
-    route_table_id = "rtb-075cb4cb775022237"
-    subnet_id      = "subnet-0770b48318cc5dda7"
+    route_table_id = "${aws_route_table.sbcntr-route-app.id}"
+    subnet_id      = "${aws_subnet.sbcntr-subnet-private-container-1a.id}"
 }
+
 resource "aws_route_table_association" "sbcntr-subnet-private-container-1c" {
-    route_table_id = "rtb-075cb4cb775022237"
-    subnet_id      = "subnet-03b6e5f6c6e0cc0cd"
+    route_table_id = "${aws_route_table.sbcntr-route-app.id}"
+    subnet_id      = "${aws_subnet.sbcntr-subnet-private-container-1c.id}"
 }
 
 resource "aws_route_table_association" "sbcntr-subnet-private-db-1a" {
-    route_table_id = "rtb-0295d2910613f8d26"
-    subnet_id      = "subnet-0951cd8c83827bf9e"
+    route_table_id = "${aws_route_table.sbcntr-route-db.id}"
+    subnet_id      = "${aws_subnet.sbcntr-subnet-private-db-1a.id}"
 }
 
 resource "aws_route_table_association" "sbcntr-subnet-private-db-1c" {
-    route_table_id = "rtb-0295d2910613f8d26"
-    subnet_id      = "subnet-0951cd8c83827bf9e"
+    route_table_id = "${aws_route_table.sbcntr-route-db.id}"
+    subnet_id      = "${aws_subnet.sbcntr-subnet-private-db-1c.id}"
 }
 
 resource "aws_route_table_association" "sbcntr-subnet-public-management-1a" {
-    route_table_id = "rtb-0d26b619f4cce982a"
-    subnet_id      = "subnet-009c2e25ecaea1e9c"
+    route_table_id = "${aws_route.sbcntr-route-ingress.id}"
+    subnet_id      = "${aws_subnet.sbcntr-subnet-public-management-1a.id}"
 }
 
 resource "aws_route_table_association" "sbcntr-subnet-public-management-1c" {
-    route_table_id = "rtb-0d26b619f4cce982a"
-    subnet_id      = "subnet-0c7b916b46c39f02c"
+    route_table_id = "${aws_route.sbcntr-route-ingress.id}"
+    subnet_id      = "${aws_subnet.sbcntr-subnet-public-management-1c.id}"
 }
