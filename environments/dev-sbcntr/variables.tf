21e2af9cee9eda930677ad3a017a3ce5a3a10404
Masato Kataoka <kata.masa@gmail.com>
2023-05-07 01:58:40 +0900

    ディレクトリ名変更

--- environments/dev-sbcntr/variables.tf
+++ environments/dev-sbcntr/variables.tf
@@ -2,3 +2,5 @@ variable "sbcntrVpc_id" {}
 variable "egress_subnet_ids" {}
 variable "egress_security_groups" {}
 variable "app_route_ids" {}
+variable "internal_security_groups" {}
+variable "container_subnets" {}
